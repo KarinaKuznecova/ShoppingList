43d00fbfad6445b7ae70c2a691d3bddea9ab4e96
KarinaKuznecova <rbthio@gmail.com>
2019-05-21 11:26:40 +0300

    Validator refactored

--- src/com/javaguru/shoppinglist/Validator.java
+++ src/com/javaguru/shoppinglist/Validator.java
@@ -5,54 +5,26 @@ import java.math.BigDecimal;
 class Validator {
 
     boolean validateProduct(Product product) {
-        return validateMinLength(product.getName()) &&
-                validateMaxLength(product.getName()) &&
-                validateMinDiscount(product.getDiscount()) &&
-                validateMaxDiscount(product.getDiscount()) &&
-                validateMinPrice(product.getPrice());
-    }
-
-    boolean validatePrice(BigDecimal price) {
-        return price.doubleValue() >= 0;
+        return validateName(product.getName()) &&
+                validateDiscount(product.getDiscount()) &&
+                validatePrice(product.getPrice());
     }
 
     boolean validateName(String name) {
-        return name.length() >= 3 && name.length() <= 30;
+        if (name.length() >= 3 && name.length() <= 30) {
+            return true;
+        }
+        throw new ValidationException("Name length should be more than 3 symbols and less than 30");
     }
 
     boolean validateDiscount(BigDecimal discount) {
-        return discount.doubleValue() >=0 && discount.doubleValue() <=80;
-    }
-
-    private boolean validateMinLength(String name) {
-        if (name.length() >= 3) {
+        if (discount.doubleValue() >=0 && discount.doubleValue() <=80) {
             return true;
         }
-        throw new ValidationException("Name length should be more than 3 symbols");
+        throw new ValidationException("Discount must be more than 0% and less than 80%");
     }
 
-    private boolean validateMaxLength(String name) {
-        if (name.length() <= 30) {
-            return true;
-        }
-        throw new ValidationException("Name length should be less than 30 symbols");
-    }
-
-    private boolean validateMinDiscount(BigDecimal discount) {
-        if (discount.doubleValue() >= 0) {
-            return true;
-        }
-        throw new ValidationException("Discount must be more than 0%");
-    }
-
-    private boolean validateMaxDiscount(BigDecimal discount) {
-        if (discount.doubleValue() <= 80) {
-            return true;
-        }
-        throw new ValidationException("Discount must be less than 80%");
-    }
-
-    private boolean validateMinPrice(BigDecimal price) {
+    boolean validatePrice(BigDecimal price) {
         if (price.doubleValue() >= 0) {
             return true;
         }
