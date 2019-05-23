cd75b1a1447553c88559a2c5ac7ce10878c4127b
KarinaKuznecova <rbthio@gmail.com>
2019-05-23 10:26:27 +0300

    Validator only throws exceptions

--- src/com/javaguru/shoppinglist/Validator.java
+++ src/com/javaguru/shoppinglist/Validator.java
@@ -4,31 +4,28 @@ import java.math.BigDecimal;
 
 class Validator {
 
-    boolean validateProduct(Product product) {
-        return validateName(product.getName()) &&
-                validateDiscount(product.getDiscount()) &&
-                validatePrice(product.getPrice());
+    void validateProduct(Product product) {
+        validateName(product.getName());
+        validateDiscount(product.getDiscount());
+        validatePrice(product.getPrice());
     }
 
-    boolean validateName(String name) {
-        if (name.length() >= 3 && name.length() <= 30) {
-            return true;
+    void validateName(String name) {
+        if (name.length() < 3 && name.length() > 30) {
+            throw new ValidationException("Name length should be more than 3 symbols and less than 30");
         }
-        throw new ValidationException("Name length should be more than 3 symbols and less than 30");
     }
 
-    boolean validateDiscount(BigDecimal discount) {
+    void validateDiscount(BigDecimal discount) {
         BigDecimal maxDiscount = new BigDecimal(80);
-        if (discount.compareTo(BigDecimal.ZERO) >= 0 && discount.compareTo(maxDiscount) <=0) {
-            return true;
+        if (discount.compareTo(BigDecimal.ZERO) < 0 || discount.compareTo(maxDiscount) > 0) {
+            throw new ValidationException("Discount must be more than 0% and less than 80%");
         }
-        throw new ValidationException("Discount must be more than 0% and less than 80%");
     }
 
-    boolean validatePrice(BigDecimal price) {
-        if (price.compareTo(BigDecimal.ZERO) > 0) {
-            return true;
+    void validatePrice(BigDecimal price) {
+        if (price.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new ValidationException("Price must be more than 0");
         }
-        throw new ValidationException("Price must be more than 0");
     }
 }
