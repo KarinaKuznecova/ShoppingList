05301309720f8efd5ee1b5c3b0674526d22361da
Jelena Avramenko <jelena.avramenko@accenture.com>
2019-06-12 21:43:25 +0300

    separation and maven

---
+++ src/test/java/com/javaguru/shoppinglist/service/validation/ProductNameValidationRuleTest.java
@@ -0,0 +1,12 @@
+package com.javaguru.shoppinglist.service.validation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductNameValidationRuleTest {
+
+    @Test
+    public void validate() {
+    }
+}
