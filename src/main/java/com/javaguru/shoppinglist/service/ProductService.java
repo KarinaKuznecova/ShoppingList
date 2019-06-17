a44fff65f0a7d0155ce0adc212da10584753977c
Jelena Avramenko <jelena.avramenko@accenture.com>
2019-06-17 17:05:04 +0300

    repository

--- src/main/java/com/javaguru/shoppinglist/service/ProductService.java
+++ src/main/java/com/javaguru/shoppinglist/service/ProductService.java
@@ -1,21 +1,21 @@
 package com.javaguru.shoppinglist.service;
 
 import com.javaguru.shoppinglist.domain.Product;
-import com.javaguru.shoppinglist.repository.ProductInMemoryRepository;
+import com.javaguru.shoppinglist.repository.InMemoryRepository;
 import com.javaguru.shoppinglist.service.validation.ProductValidationService;
 
 public class ProductService {
-    private ProductInMemoryRepository repository = new ProductInMemoryRepository();
+    private InMemoryRepository repository = new InMemoryRepository();
     private ProductValidationService validationService = new ProductValidationService();
 
     public Long createProduct(Product product) {
         validationService.validate(product);
-        Product createdProduct = repository.insert(product);
+        Product createdProduct = repository.add(product);
         return createdProduct.getId();
     }
 
     public Product findProductById(Long id) {
-        return repository.findProductById(id);
+        return repository.getProductById(id);
     }
 
     public Product deleteProductById(Long id) {
