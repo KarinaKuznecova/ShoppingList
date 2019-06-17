a22fcf66349a6f83891f66fb5e98897882d6818c
Jelena Avramenko <jelena.avramenko@accenture.com>
2019-06-17 17:05:04 +0300

    repository

---
+++ src/main/java/com/javaguru/shoppinglist/repository/InMemoryRepository.java
@@ -0,0 +1,46 @@
+package com.javaguru.shoppinglist.repository;
+
+import com.javaguru.shoppinglist.domain.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryRepository {
+    private Map<Long, Product> productRepository = new HashMap<>();
+    private Long productIdSequence = 0L;
+
+    public Product add(Product product) {
+        product.setId(productIdSequence++);
+        productRepository.put(product.getId(), product);
+        return product;
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.get(id);
+    }
+
+    public Product removeProductById(Long id) {
+        return productRepository.remove(id);
+    }
+
+    public void removeAllProducts() {
+        productRepository.clear();
+        productIdSequence = 0L;
+    }
+
+    public void printAll() {
+        for (Product i : productRepository.values()) {
+            System.out.println(i);
+        }
+    }
+
+    public boolean containsProduct(Product product) {
+        return productRepository.containsValue(product);
+    }
+
+    public long getStorageSize() {
+        return productRepository.size();
+    }
+
+
+}
